e9c2d4a9cbe2a29c4fa9912e610671417c5a5ada
skplunkerin <skplunkerin@pm.me>
2023-02-09 16:39:44 -0700

    Create Vending Machine v1
    * Add ENV variables to `.env-sample`
    
    * Delete Hardhat init files from `contracts/`, `tests/`, and `scripts/`
    
    * Update `hardhat.config.js` with Goerli network and etherscan
      configurations

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.TESTNET_ALCHEMY_RPC_URL,
+      accounts: [process.env.TESTNET_WALLET_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY,
+  },
 };
