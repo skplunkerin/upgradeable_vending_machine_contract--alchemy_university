155e62dfb4bb8e6316d5b56948755bc4f5a6e40f
Skplunkerin <skplunkerin@pm.me>
2023-02-09 18:08:45 -0700

    Create Vending Machine v1 (#1)
    * Create Vending Machine v1
    
    * Add ENV variables to `.env-sample`
    
    * Delete Hardhat init files from `contracts/`, `tests/`, and `scripts/`
    
    * Update `hardhat.config.js` with Goerli network and etherscan
      configurations
    
    * * Require new packages to `hardhat.config.js`
    
    * Create Github Dependabot configuration file
    
    * Install new packages:
      - @nomiclabs/hardhat-ethers
      - @nomiclabs/hardhat-etherscan
      - @openzeppelin/contracts-upgradeable
      - @openzeppelin/hardhat-upgrades
    
    * Create new `VendingMachineV1.sol` contract
    
    * * Add new `deployProxy.js` script
      - Update `package.json` `deploy_*` scripts to use correct file name
    
    * Update deploy_local script to use `--network localhost`
      - Update defaultNetwork to localhost in `hardhat.config.js` as well
    
    * Add new `verify_goerli` script to `package.json`
    
    * Deploy v1 contract to Goerli:
      - Include newly created `.openzeppelin/goerli.json` file per
        OpenZeppelins recomendation: https://docs.openzeppelin.com/upgrades-plugins/1.x/network-files
      - Update `README.md` with contract addresses
    
    * * Fix Dependabot package-ecosystem to `npm`

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require("@openzeppelin/hardhat-upgrades");
+require("@nomiclabs/hardhat-ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "localhost",
+  networks: {
+    goerli: {
+      url: process.env.TESTNET_ALCHEMY_RPC_URL,
+      accounts: [process.env.TESTNET_WALLET_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY,
+  },
 };
